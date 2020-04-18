4d38169926e890688db02c072e540424f24c7dce
Hyunsujk <hyunsujk@gmail.com>
2020-04-17 21:43:59 -0500

    when submit button is clicked on the front end, the value is save to the database and brings user back to home page. test good.

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -4,13 +4,24 @@ const pool = require("../modules/pool");
 
 router.post("/", (req, res) => {
   console.log(req.body);
-  //   const queryString = `INSERT INTO "feedback"
-  //     (feeling, understanding, support, comments)
-  //     VALUES ($1,$2,$3,$4);`;
+  const queryString = `INSERT INTO "feedback"
+      (feeling, understanding, support, comments)
+      VALUES ($1,$2,$3,$4);`;
 
-  //     pool.query(queryString, [
-  //         req.body.
-  //     ])
+  pool
+    .query(queryString, [
+      req.body.feeling,
+      req.body.understanding,
+      req.body.support,
+      req.body.comment,
+    ])
+    .then((response) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
