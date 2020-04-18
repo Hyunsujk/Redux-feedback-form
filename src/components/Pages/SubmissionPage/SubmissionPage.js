2b40170019c9237315fc3442e8fbf3d14ab7fc1e
Hyunsujk <hyunsujk@gmail.com>
2020-04-18 12:05:37 -0500

    styled card

--- src/components/Pages/SubmissionPage/SubmissionPage.js
+++ src/components/Pages/SubmissionPage/SubmissionPage.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import { sizing } from "@material-ui/system";
+import "./SubmissionPage.css";
 
 class SubmissionPage extends Component {
   handleClick = (event) => {
@@ -13,20 +16,22 @@ class SubmissionPage extends Component {
     const classes = this.props;
 
     return (
-      <div>
-        <div>
-          <h2>Feedback!</h2>
-        </div>
-        <div className={classes.root}>
-          <h2>Thank You!</h2>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={this.handleClick}
-          >
-            Leave New Feedback
-          </Button>
-        </div>
+      <div className="center">
+        <Card className="card">
+          <div>
+            <h2>Feedback!</h2>
+          </div>
+          <div className={classes.root}>
+            <h2>Thank You!</h2>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleClick}
+            >
+              Leave New Feedback
+            </Button>
+          </div>
+        </Card>
       </div>
     );
   }
